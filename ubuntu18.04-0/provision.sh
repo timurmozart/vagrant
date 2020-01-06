4012a6ac771b07bf6d326fa63a45e610555e727f
Tymur Riazantsev <triaz@macmini3634.softservecom.com>
2020-01-06 11:23:25 +0200

    rsyslog experiments

---
+++ ubuntu18.04-0/provision.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt autoremove -y
+sudo sed -i 's/ubuntu-bionic/rsyslog-0/g' /etc/hostname
+sudo apt-get install chrony mc -y
+sudo systemctl enable chrony
+sudo systemctl start chrony
+sudo sed -i 's/#module(load="imudp")/module(load="imudp")/g' /etc/rsyslog.conf
+sudo sed -i 's/#input(type="imudp" port="514")/input(type="imudp" port="514")/g' /etc/rsyslog.conf
+sudo sed -i 's/#module(load="imtcp")/module(load="imtcp")/g' /etc/rsyslog.conf
+sudo sed -i 's/#input(type="imtcp" port="514")/input(type="imtcp" port="514")/g' /etc/rsyslog.conf
+
+sudo reboot now
---
+++ ubuntu18.04-1/provision.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt autoremove -y
+sudo sed -i 's/ubuntu-bionic/rsyslog-1/g' /etc/hostname
+sudo apt-get install chrony -y
+sudo systemctl enable chrony
+sudo systemctl start chrony
+sudo sed -i 's/#module(load="imudp")/module(load="imudp")/g' /etc/rsyslog.conf
+sudo sed -i 's/#input(type="imudp" port="514")/input(type="imudp" port="514")/g' /etc/rsyslog.conf
+sudo sed -i 's/#module(load="imtcp")/module(load="imtcp")/g' /etc/rsyslog.conf
+sudo sed -i 's/#input(type="imtcp" port="514")/input(type="imtcp" port="514")/g' /etc/rsyslog.conf
+sudo echo "*.* @@192.168.10.10:514" > /etc/rsyslog.d/all.conf
+
+
+
+sudo reboot now
+
