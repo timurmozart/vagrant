aff8cff4f277d51446102b85d623ed6228f41bf5
timurmozart <47573645+timurmozart@users.noreply.github.com>
2020-01-07 13:00:11 +0200

    07.01.20

--- ubuntu18.04-0/provision.sh
+++ ubuntu18.04-0/provision.sh
@@ -3,12 +3,13 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt autoremove -y
 sudo sed -i 's/ubuntu-bionic/rsyslog-0/g' /etc/hostname
-sudo apt-get install chrony mc -y
-sudo systemctl enable chrony
-sudo systemctl start chrony
+sudo apt-get install mc -y
+# sudo systemctl enable chrony
+# sudo systemctl start chrony
 sudo sed -i 's/#module(load="imudp")/module(load="imudp")/g' /etc/rsyslog.conf
 sudo sed -i 's/#input(type="imudp" port="514")/input(type="imudp" port="514")/g' /etc/rsyslog.conf
 sudo sed -i 's/#module(load="imtcp")/module(load="imtcp")/g' /etc/rsyslog.conf
 sudo sed -i 's/#input(type="imtcp" port="514")/input(type="imtcp" port="514")/g' /etc/rsyslog.conf
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 
 sudo reboot now
