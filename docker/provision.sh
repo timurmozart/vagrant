3fd523c49b2f63b8f5c4ee50dbb9c251cfadc6c0
Tymur Riazantsev <triaz@softserveinc.com>
2020-01-10 17:22:24 +0200

    10.01.20

---
+++ docker/provision.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo yum check-update
+curl -fsSL https://get.docker.com/ | sh
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo echo "docker-host" > /etc/hostname
+sudo yum install mc tree tmux -y
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sudo sed -i 's/#PermitRootLogin yes/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sudo sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
+# sudo cp -r /mnt/host_machine/.ssh/ /home/vagrant/.ssh/
+sudo systemctl restart sshd
+# sudo ssh-keygen
--- ubuntu18.04-0/provision.sh
+++ ubuntu18.04-0/provision.sh
@@ -3,7 +3,7 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt autoremove -y
 sudo sed -i 's/ubuntu-bionic/rsyslog-0/g' /etc/hostname
-sudo apt-get install mc -y
+sudo apt-get install mc tree -y
 # sudo systemctl enable chrony
 # sudo systemctl start chrony
 sudo sed -i 's/#module(load="imudp")/module(load="imudp")/g' /etc/rsyslog.conf
