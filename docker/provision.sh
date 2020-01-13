0840d82cb7b43df20135983efefa82420ab4996e
Tymur Riazantsev <triaz@softserveinc.com>
2020-01-13 17:11:02 +0200

    docker-compose add

--- docker/provision.sh
+++ docker/provision.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-sudo yum check-update
-curl -fsSL https://get.docker.com/ | sh
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo echo "docker-host" > /etc/hostname
-sudo yum install mc tree tmux -y
+sudo yum check-update \
+curl -fsSL https://get.docker.com/ | sh \
+sudo systemctl start docker \
+sudo systemctl enable docker \
+sudo echo "docker-host" > /etc/hostname \
+sudo yum install mc tree tmux -y 
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 sudo sed -i 's/#PermitRootLogin yes/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sudo sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
